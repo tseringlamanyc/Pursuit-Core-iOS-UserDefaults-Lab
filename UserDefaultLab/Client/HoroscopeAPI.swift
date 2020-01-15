81db4adfbb74e32ef9060adab691a04c0b6a6c13
Tsering Lama <tseringlama@pursuit.org>
2020-01-15 10:09:21 -0500

    set up the api and model for the postman url

--- UserDefaultLab/Client/HoroscopeAPI.swift
+++ UserDefaultLab/Client/HoroscopeAPI.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct HoroscopeAPI {
+    static func getHoroscope(userSign: String, completionHandler: @escaping (Result<TodaysSign,AppError>) -> ()) {
+        let endpointURL = "http://sandipbgt.com/theastrologer/api/horoscope/\(userSign)/today"
+        guard let url = URL(string: endpointURL) else {
+            completionHandler(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completionHandler(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let horoData = try JSONDecoder().decode(TodaysSign.self, from: data)
+                    completionHandler(.success(horoData))
+                } catch {
+                    completionHandler(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
--- UserDefaultLab/Model/Json.swift
+++ UserDefaultLab/Model/Json.swift
@@ -7,3 +7,7 @@
 //
 
 import Foundation
+
+struct TodaysSign: Codable {
+    let horoscope: String
+}
